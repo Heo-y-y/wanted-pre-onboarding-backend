7ae3bc2d3c46d0c3fc8b265609ee712b9ad380e8
Heoyungyoung <gjdbs2597@naver.com>
2023-08-12 16:54:23 +0900

    feat: 스웨거 적용

--- src/main/java/com/wanted/backend/controller/UserController.java
+++ src/main/java/com/wanted/backend/controller/UserController.java
@@ -2,6 +2,9 @@ package com.wanted.backend.controller;
 
 import com.wanted.backend.dto.SignUpDto;
 import com.wanted.backend.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,11 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@Api(tags = "userController")
 @RequestMapping("/api")
 @RestController
 public class UserController {
     private final UserService userService;
 
+    @ApiOperation(value = "회원가입")
     @PostMapping("/signup")
     public ResponseEntity<Void> signUp(@Validated @RequestBody SignUpDto signUp) {
         userService.registerUser(signUp.getEmail(), signUp.getPassword());
