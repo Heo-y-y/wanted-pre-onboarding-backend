1d494dd5a91ba0173cfe2d8676ec404dec3543e7
Heoyungyoung <gjdbs2597@naver.com>
2023-08-15 17:57:04 +0900

    init: 삭제

--- src/main/java/com/wanted/backend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wanted.backend.repository;
-
-import com.wanted.backend.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-}
